954eab300d435a00f04e0e908d26d6bd0db0d8a7
Graham Campbell <graham@mineuk.com>
2013-11-04 18:19:42 +0000

    Complete api coverage

--- src/GrahamCampbell/CloudFlareAPI/Classes/CloudFlareAPI.php
+++ src/GrahamCampbell/CloudFlareAPI/Classes/CloudFlareAPI.php
@@ -93,19 +93,19 @@ class CloudFlareAPI extends CoreAPI {
     public function api_stats($interval = 20) {
         return $this->request(array(
             'a'        => 'stats',
-            'interval' => $interval,
+            'interval' => $interval
         ), true, 60);
     }
 
     public function api_zone_load_multi() {
         return $this->request(array(
-            'a' => 'zone_load_multi',
+            'a' => 'zone_load_multi'
         ), false, 5);
     }
 
     public function api_rec_load_all() {
         return $this->request(array(
-            'a' => 'rec_load_all',
+            'a' => 'rec_load_all'
         ), true, 5);
     }
 
@@ -117,7 +117,7 @@ class CloudFlareAPI extends CoreAPI {
         }
         return $this->request(array(
             'a'     => 'zone_check',
-            'zones' => implode(',', $values),
+            'zones' => implode(',', $values)
         ), false, 5);
     }
 
@@ -126,20 +126,128 @@ class CloudFlareAPI extends CoreAPI {
             'a'     => 'zone_ips',
             'hours' => $hours,
             'class' => $class,
-            'geo'   => (int)$geo,
+            'geo'   => (int)$geo
         ), true, 5);
     }
 
     public function api_ip_lkup($ip) {
         return $this->request(array(
             'a'  => 'ip_lkup',
-            'ip' => $ip,
+            'ip' => $ip
         ), false, 5);
     }
 
     public function api_zone_settings() {
         return $this->request(array(
-            'a' => 'zone_settings',
+            'a' => 'zone_settings'
         ), true, 5);
+    }
+
+    public function api_sec_lvl($v) {
+        return $this->request(array(
+            'a' => 'sec_lvl',
+            'v' => $v
+        ));
+    }
+
+    public function api_cache_lvl($v) {
+        return $this->request(array(
+            'a' => 'cache_lvl',
+            'v' => $v
+        ));
+    }
+
+    public function api_devmode($v) {
+        return $this->request(array(
+            'a' => 'zone_settings',
+            'v' => (int)$v
+        ));
+    }
+
+    public function api_fpurge_ts() {
+        return $this->request(array(
+            'a' => 'fpurge_ts',
+            'v' => (int)true
+        ));
+    }
+
+    public function api_zone_file_purge($url) {
+        return $this->request(array(
+            'a'   => 'zone_file_purge',
+            'url' => $url
+        ));
+    }
+
+    public function api_zone_grab($zid) {
+        return $this->request(array(
+            'a'   => 'zone_grab',
+            'zid' => $zid
+        ), false);
+    }
+
+    public function api_wl($key) {
+        return $this->request(array(
+            'a'   => 'wl',
+            'key' => $key
+        ), false);
+    }
+
+    public function api_ban($key) {
+        return $this->request(array(
+            'a'   => 'ban',
+            'key' => $key
+        ), false);
+    }
+
+    public function api_nul($key) {
+        return $this->request(array(
+            'a'   => 'nul',
+            'key' => $key
+        ), false);
+    }
+
+    public function api_ipv46($v) {
+        return $this->request(array(
+            'a' => 'ipv46',
+            'v' => (int)$v
+        ));
+    }
+
+    public function api_async($v) {
+        return $this->request(array(
+            'a' => 'async',
+            'v' => $v
+        ));
+    }
+
+    public function api_minify($v) {
+        return $this->request(array(
+            'a' => 'minify',
+            'v' => $v
+        ));
+    }
+
+    public function api_mirage2($v) {
+        return $this->request(array(
+            'a' => 'mirage2',
+            'v' => (int)$v
+        ));
+    }
+
+    public function api_rec_new(array $data) {
+        $data['a'] = 'rec_new';
+        return $this->request($data);
+    }
+
+    public function api_rec_edit(array $data) {
+        $data['a'] = 'rec_edit';
+        return $this->request($data);
+    }
+
+    public function api_rec_delete($id) {
+        return $this->request(array(
+            'a'  => 'rec_delete',
+            'id' => $id
+        ));
     }
 }
