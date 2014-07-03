f4bd3db74167b97d8cdadcc6e06cea9050efc8f1
Graham Campbell <graham@mineuk.com>
2014-07-03 17:58:06 +0100

    Added the rest of the api

--- src/Factories/ClientFactory.php
+++ src/Factories/ClientFactory.php
@@ -84,6 +84,389 @@ class ClientFactory extends AbstractClientFactory
                         )
                     )
                 ),
+                'zoneLoadMulti' => array(
+                    'httpMethod' => 'GET',
+                    'uri' => '?a=zone_load_multi',
+                    'responseModel' => 'jsonResponse',
+                    'parameters' => array(
+                        // none
+                    )
+                ),
+                'recLoadAll' => array(
+                    'httpMethod' => 'GET',
+                    'uri' => '?a=rec_load_all',
+                    'responseModel' => 'jsonResponse',
+                    'parameters' => array(
+                        'z' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        ),
+                        'o' => array(
+                            'type' => 'int',
+                            'location' => 'query'
+                        )
+                    )
+                ),
+                'zoneCheck' => array(
+                    'httpMethod' => 'GET',
+                    'uri' => '?a=zone_check',
+                    'responseModel' => 'jsonResponse',
+                    'parameters' => array(
+                        'zones' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        )
+                    )
+                ),
+                'zoneIps' => array(
+                    'httpMethod' => 'GET',
+                    'uri' => '?a=zone_ips',
+                    'responseModel' => 'jsonResponse',
+                    'parameters' => array(
+                        'z' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        ),
+                        'hours' => array(
+                            'type' => 'int',
+                            'location' => 'query'
+                        ),
+                        'class' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        ),
+                        'geo' => array(
+                            'type' => 'int',
+                            'location' => 'query'
+                        )
+                    )
+                ),
+                'ipLkup' => array(
+                    'httpMethod' => 'GET',
+                    'uri' => '?a=ip_lkup',
+                    'responseModel' => 'jsonResponse',
+                    'parameters' => array(
+                        'ip' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        )
+                    )
+                ),
+                'zoneSettings' => array(
+                    'httpMethod' => 'GET',
+                    'uri' => '?a=zone_settings',
+                    'responseModel' => 'jsonResponse',
+                    'parameters' => array(
+                        'z' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        )
+                    )
+                ),
+                'secLvl' => array(
+                    'httpMethod' => 'POST',
+                    'uri' => '?a=sec_lvl',
+                    'responseModel' => 'jsonResponse',
+                    'parameters' => array(
+                        'z' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        ),
+                        'v' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        )
+                    )
+                ),
+                'cacheLvl' => array(
+                    'httpMethod' => 'POST',
+                    'uri' => '?a=cache_lvl',
+                    'responseModel' => 'jsonResponse',
+                    'parameters' => array(
+                        'z' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        ),
+                        'v' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        )
+                    )
+                ),
+                'devmode' => array(
+                    'httpMethod' => 'POST',
+                    'uri' => '?a=devmode',
+                    'responseModel' => 'jsonResponse',
+                    'parameters' => array(
+                        'z' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        ),
+                        'v' => array(
+                            'type' => 'int',
+                            'location' => 'query'
+                        )
+                    )
+                ),
+                'fpurgeTs' => array(
+                    'httpMethod' => 'POST',
+                    'uri' => '?a=fpurge_ts&v=1',
+                    'responseModel' => 'jsonResponse',
+                    'parameters' => array(
+                        'z' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        )
+                    )
+                ),
+                'zoneFilePurge' => array(
+                    'httpMethod' => 'POST',
+                    'uri' => '?a=zone_file_purge',
+                    'responseModel' => 'jsonResponse',
+                    'parameters' => array(
+                        'z' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        ),
+                        'url' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        )
+                    )
+                ),
+                'zoneGrab' => array(
+                    'httpMethod' => 'POST',
+                    'uri' => '?a=zone_grab',
+                    'responseModel' => 'jsonResponse',
+                    'parameters' => array(
+                        'zid' => array(
+                            'type' => 'int',
+                            'location' => 'query'
+                        )
+                    )
+                ),
+                'wl' => array(
+                    'httpMethod' => 'POST',
+                    'uri' => '?a=wl',
+                    'responseModel' => 'jsonResponse',
+                    'parameters' => array(
+                        'key' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        )
+                    )
+                ),
+                'ban' => array(
+                    'httpMethod' => 'POST',
+                    'uri' => '?a=ban',
+                    'responseModel' => 'jsonResponse',
+                    'parameters' => array(
+                        'key' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        )
+                    )
+                ),
+                'nul' => array(
+                    'httpMethod' => 'POST',
+                    'uri' => '?a=nul',
+                    'responseModel' => 'jsonResponse',
+                    'parameters' => array(
+                        'key' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        )
+                    )
+                ),
+                'ipv46' => array(
+                    'httpMethod' => 'POST',
+                    'uri' => '?a=ipv46',
+                    'responseModel' => 'jsonResponse',
+                    'parameters' => array(
+                        'z' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        ),
+                        'v' => array(
+                            'type' => 'int',
+                            'location' => 'query'
+                        )
+                    )
+                ),
+                'async' => array(
+                    'httpMethod' => 'POST',
+                    'uri' => '?a=async',
+                    'responseModel' => 'jsonResponse',
+                    'parameters' => array(
+                        'z' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        ),
+                        'v' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        )
+                    )
+                ),
+                'minify' => array(
+                    'httpMethod' => 'POST',
+                    'uri' => '?a=minify',
+                    'responseModel' => 'jsonResponse',
+                    'parameters' => array(
+                        'z' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        ),
+                        'v' => array(
+                            'type' => 'int',
+                            'location' => 'query'
+                        )
+                    )
+                ),
+                'mirage2' => array(
+                    'httpMethod' => 'POST',
+                    'uri' => '?a=mirage2',
+                    'responseModel' => 'jsonResponse',
+                    'parameters' => array(
+                        'z' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        ),
+                        'v' => array(
+                            'type' => 'int',
+                            'location' => 'query'
+                        )
+                    )
+                ),
+                'recNew' => array(
+                    'httpMethod' => 'POST',
+                    'uri' => '?a=rec_new',
+                    'responseModel' => 'jsonResponse',
+                    'parameters' => array(
+                        'z' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        ),
+                        'type' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        ),
+                        'name' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        ),
+                        'content' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        ),
+                        'ttl' => array(
+                            'type' => 'int',
+                            'location' => 'query'
+                        ),
+                        'prio' => array(
+                            'type' => 'int',
+                            'location' => 'query'
+                        ),
+                        'service' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        ),
+                        'srvname' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        ),
+                        'protocol' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        ),
+                        'weight' => array(
+                            'type' => 'int',
+                            'location' => 'query'
+                        ),
+                        'port' => array(
+                            'type' => 'int',
+                            'location' => 'query'
+                        ),
+                        'target' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        )
+                    )
+                ),
+                'recEdit' => array(
+                    'httpMethod' => 'POST',
+                    'uri' => '?a=rec_edit',
+                    'responseModel' => 'jsonResponse',
+                    'parameters' => array(
+                        'z' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        ),
+                        'type' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        ),
+                        'id' => array(
+                            'type' => 'int',
+                            'location' => 'query'
+                        ),
+                        'name' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        ),
+                        'content' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        ),
+                        'ttl' => array(
+                            'type' => 'int',
+                            'location' => 'query'
+                        ),
+                        'prio' => array(
+                            'type' => 'int',
+                            'location' => 'query'
+                        ),
+                        'service' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        ),
+                        'srvname' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        ),
+                        'protocol' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        ),
+                        'weight' => array(
+                            'type' => 'int',
+                            'location' => 'query'
+                        ),
+                        'port' => array(
+                            'type' => 'int',
+                            'location' => 'query'
+                        ),
+                        'target' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        )
+                    )
+                ),
+                'recDelete' => array(
+                    'httpMethod' => 'POST',
+                    'uri' => '?a=rec_delete',
+                    'responseModel' => 'jsonResponse',
+                    'parameters' => array(
+                        'z' => array(
+                            'type' => 'string',
+                            'location' => 'query'
+                        ),
+                        'id' => array(
+                            'type' => 'int',
+                            'location' => 'query'
+                        )
+                    )
+                )
             ),
             'models' => array(
                 'jsonResponse' => array(
